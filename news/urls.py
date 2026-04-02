2f22e10237b9e0c6c520c47c25744e4cdab99637
asafusuion-ux <asafusuion@gmail.com>
2026-04-03 00:14:19 +0600

    modified html, added views

--- news/urls.py
+++ news/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('category/<slug:slug>/', views.category_posts, name='category_posts'),
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
